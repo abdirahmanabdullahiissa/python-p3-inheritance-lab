6a769c3038a78a0126f605445337f702bc2a052e
abdirahmanabdullahiissa <abdirahman.abdullahi@student.moringaschool.com>
2024-01-09 20:12:37 +0300

    done

--- lib/student.py
+++ lib/student.py
@@ -1,8 +1,26 @@
 #!/usr/bin/env python
+#!/usr/bin/env python
 
 from user import User
 
+
 class Student(User):
-    
-    def learn(self):
-        pass
+    def __init__(self, first_name, last_name, knowledge=None):
+        super().__init__(first_name, last_name)
+        if knowledge is None:
+            self.knowledge = []
+        else:
+            self.knowledge = knowledge
+
+    def learn(self, unit):
+        if unit not in self.knowledge:
+            self.knowledge.append(unit)
+
+
+# knowledge = [
+#     "str is a data type in Python",
+# ]
+# s = Student("Halani", "hikey", knowledge)
+# s.learn(
+#     "JavaScript async web request",
+# )
